d40651be718edda369b1f7a3b06fa19b4da20818
BadWasabeee <63248409+BadWasabeee@users.noreply.github.com>
2020-10-09 11:55:52 -0500

    Fixed player profile name with rank...

--- main.py
+++ main.py
@@ -47,50 +47,11 @@ def profile():
     network_rank = hypixel.Player(raw_username).getRank()['rank']
     network_level = player.get_level()
     network_exp = str(player.get_exp())
-    stats = player.get_stats(hypixelapi.GameType.SKYBLOCK)
-    for profiles in stats:
-        print(stats['profiles'])
-    # number_of_profiles = player.get_stats(hypixelapi.GameType.SKYBLOCK)['profiles'][uuid]['cute_name']
-    # profiles = player.get_stats(hypixelapi.GameType.SKYBLOCK)['profiles']['profile_id']
-    # print(profiles)
-    # for i in profilxes:
-    #     print(profiles['cute_name'])
     if not uuid:
         session['logged_in'] = False
     else:
-        if str(network_rank).lower() == 'youtube':
-            rank_color = '#FF5555'
-        elif str(network_rank).lower() == "admin":
-            rank_color = '#FF5555'
-        elif str(network_rank).lower() == "mod":
-            rank_color = '#00AA00'
-        elif str(network_rank).lower() == "helper":
-            rank_color = '#5555FF'
-        elif str(network_rank).lower() == "Superstar":
-            rank_color = '#FFAA00'
-        elif str(network_rank).lower() == "mvp_plus":
-            rank_color = '#55FFFF'
-        elif str(network_rank).lower() == "mvp":
-            rank_color = '#55FFFF'
-        elif str(network_rank).lower() == "vip_plus":
-            rank_color = '#55FF55'
-        elif str(network_rank).lower() == "vip":
-            rank_color = '#55FF55'
-        elif str(network_rank).lower() == "none":
-            rank_color = '#AAAAAA'
-
-        if not str(network_rank).lower() == 'none':
-            rank_tag = str('[' + str(network_rank) + ']').upper()
-        else:
-            rank_tag = ''
-
-        print(network_exp)
-
-        views = str(0)
-
-        return render_template('profile.html', username=name, network_rank=network_rank, ranktag=rank_tag, uuid=uuid,
-                               network_level=network_level,
-                               rankcolor=rank_color)
+        return render_template('profile.html', username=name, network_rank=network_rank, uuid=uuid,
+                               network_level=network_level, network_exp=network_exp)
     return home()
 
 
